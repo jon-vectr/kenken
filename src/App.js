e8513b367329aa332b9cd7e37edb624ab5852e9b
jonathan cole <118086845+jon-vectr@users.noreply.github.com>
2023-01-15 04:20:41 +0000

    added temporary grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { Box, Container, Typography } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import React from 'react';
 import './App.css';
 
 function App() {
+  let numbers = ['1+', '2-', '3-', '4÷', '5x', '6+', '7+', '8-', '9+'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ width: '100vw', height: '100vh' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', alignContent: 'center', width: '100%', height: '100%', gap: 4 }}>
+        <Typography variant='h5'>
+          kenken.io
+        </Typography>
+        <Grid2 container columns={12} sx={{
+            '--Grid-borderWidth': '4px',
+            borderTop: 'var(--Grid-borderWidth) solid',
+            borderLeft: 'var(--Grid-borderWidth) solid',
+            borderColor: '#000000',
+            '& > div': {
+              borderRight: 'var(--Grid-borderWidth) solid',
+              borderBottom: 'var(--Grid-borderWidth) solid',
+              borderColor: '#000000',
+            },
+            width: '50vh',
+            height: '50vh'
+        }}>
+          {numbers.map((number) => (
+            <Grid2 item xs={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  ":hover": {
+              backgroundColor: '#e0e0e0',
+              transition: 'all 0.3s ease'
+            } }}>
+              {number}
+            </Grid2>
+          ))}
+        </Grid2>
+      </Box>
+    </Container>
   );
 }
 
